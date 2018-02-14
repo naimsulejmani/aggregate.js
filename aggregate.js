59b1ed2c324117e95e3dc0d1561fe01014a5bc6a
Naim Sulejmani <naim.sulejmani@gmail.com>
2018-02-14 11:43:02 +0100

    aggregate js - basics

---
+++ aggregate.js
@@ -0,0 +1,67 @@
+
+function GroupBy(array,columns){
+	if(!array.length || !columns.length)
+		return;
+	var array=array;
+	var columns=columns;
+	var retArr={};
+
+	var initialize=function(type)
+  	{
+  		if(type=='sum'||type=='avg'){
+  			for(var str of columns){
+		        retArr[str]=0;
+    		}
+  		}
+  		else if(type=='min' || type=='max'){
+  			for(var str of columns){
+		        retArr[str]=array[0][str];
+    		}
+  		}
+  	}
+
+	return {
+		sum:function (){	
+				initialize('sum');
+			    return array.reduce(function(a,item){
+			        for(var str of columns){
+			            retArr[str]+=item[str];
+			        }
+			        return retArr
+			    },columns);
+			},
+		min:function (){	  
+			   initialize('min');
+			      return array.reduce(function(a,item){
+			        for(var str of columns){
+			        	if(item[str]<retArr[str])
+			        		retArr[str]=item[str]
+			        }
+			        return retArr
+			    },columns);
+			},
+		max:function (){	  
+			   initialize('max');
+			      return array.reduce(function(a,item){
+			        for(var str of columns){
+			        	if(item[str]>retArr[str])
+			        		retArr[str]=item[str]
+			        }
+			        return retArr
+			    },columns);
+			},
+		avg:function (){	  
+			   initialize('avg');
+			     var retObj= array.reduce(function(a,item){
+			        for(var str of columns){
+			            retArr[str]+=item[str];
+			        }
+			        return retArr},columns);
+				 
+				 for(var str of columns){
+				        retArr[str]/=array.length;
+				    }
+				    return retArr;
+				}
+	}
+}
