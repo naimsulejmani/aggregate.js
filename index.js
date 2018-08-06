7fd55c6c8293e62b80c66f7b6fbce7fec728c4b6
Naim Sulejmani <keds13136@KEDS.kv>
2018-08-07 00:49:57 +0200

    distinct count added

--- index.js
+++ index.js
@@ -1,66 +1,78 @@
-exports.GroupBy= function (array,columns){
-	if(!array.length || !columns.length)
+export function GroupBy(array, columns) {
+	if (!array.length || !columns.length)
 		return;
-	var array=array;
-	var columns=columns;
-	var retArr={};
-
-	var initialize=function(type)
-  	{
-  		if(type=='sum'||type=='avg'){
-  			for(var str of columns){
-		        retArr[str]=0;
-    		}
-  		}
-  		else if(type=='min' || type=='max'){
-  			for(var str of columns){
-		        retArr[str]=array[0][str];
-    		}
-  		}
-  	}
-
+	var array = array;
+	var columns = columns;
+	var retArr = {};
+	var initialize = function (type) {
+		if (type == 'sum' || type == 'avg') {
+			for (var str of columns) {
+				retArr[str] = 0;
+			}
+		}
+		else if (type == 'min' || type == 'max') {
+			for (var str of columns) {
+				retArr[str] = array[0][str];
+			}
+		}
+	};
 	return {
-		sum:function (){	
-				initialize('sum');
-			    return array.reduce(function(a,item){
-			        for(var str of columns){
-			            retArr[str]+=item[str];
-			        }
-			        return retArr
-			    },columns);
-			},
-		min:function (){	  
-			   initialize('min');
-			      return array.reduce(function(a,item){
-			        for(var str of columns){
-			        	if(item[str]<retArr[str])
-			        		retArr[str]=item[str]
-			        }
-			        return retArr
-			    },columns);
-			},
-		max:function (){	  
-			   initialize('max');
-			      return array.reduce(function(a,item){
-			        for(var str of columns){
-			        	if(item[str]>retArr[str])
-			        		retArr[str]=item[str]
-			        }
-			        return retArr
-			    },columns);
-			},
-		avg:function (){	  
-			   initialize('avg');
-			     var retObj= array.reduce(function(a,item){
-			        for(var str of columns){
-			            retArr[str]+=item[str];
-			        }
-			        return retArr},columns);
-				 
-				 for(var str of columns){
-				        retArr[str]/=array.length;
-				    }
-				    return retArr;
+		sum: function () {
+			initialize('sum');
+			return array.reduce(function (a, item) {
+				for (var str of columns) {
+					retArr[str] += item[str];
 				}
-	}
+				return retArr;
+			}, columns);
+		},
+		min: function () {
+			initialize('min');
+			return array.reduce(function (a, item) {
+				for (var str of columns) {
+					if (item[str] < retArr[str])
+						retArr[str] = item[str];
+				}
+				return retArr;
+			}, columns);
+		},
+		max: function () {
+			initialize('max');
+			return array.reduce(function (a, item) {
+				for (var str of columns) {
+					if (item[str] > retArr[str])
+						retArr[str] = item[str];
+				}
+				return retArr;
+			}, columns);
+		},
+		avg: function () {
+			initialize('avg');
+			var retObj = array.reduce(function (a, item) {
+				for (var str of columns) {
+					retArr[str] += item[str];
+				}
+				return retArr;
+			}, columns);
+			for (var str of columns) {
+				retArr[str] /= array.length;
+			}
+			return retArr;
+		},
+		distinctCount:function(){
+			
+			var retObj = array.reduce(function (a, item) {
+				for (var str of columns) {
+					if(!retArr.hasOwnProperty(str)) retArr[str]={};
+					retArr[str][item[str]]=1;
+				}
+				return retArr;
+			}, columns);
+
+			for (var str of columns){
+				retArr[str]=Object.keys(retArr[str]).length;
+			}
+			return retArr;
+		}
+	};
 }
